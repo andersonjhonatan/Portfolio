72084c89feb0532d99de3c4c65afcc0e76ced7e2
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2023-07-31 01:21:12 -0300

    Organizando espaçamento do Work

--- components/Work.tsx
+++ components/Work.tsx
@@ -7,23 +7,23 @@ import {MdPreview} from 'react-icons/md';
 
 const Work = () => {
   return (
-    <div className='text-white ml-24 sm:w-[60rem] sm:h-auto justify-around flex items-center'>
-      <div className=' mt-14'>
+    <div className='text-white sm:w-full justify-around flex items-center h-full'>
+      <div>
         <p className='text-[25rem] text-[#3a3a3a79] font-thin'>&#123;</p>
       </div>
-      <div className='p-4 flex flex-col text-center gap-2 w-full'>
+      <div className='flex flex-col max-h-full text-center gap-2 w-full'>
         <h1 className='text-3xl'>Projects<span className='text-[#21d19f] text-2xl'>.</span></h1>
         {work.map((item) => (
           <div
             key={item.id}
-            className=' flex items-stretch justify-between flex-row-reverse py-4 bg-[#080808] border border-[#3a3a3a79] rounded-md text-start'
+            className=' flex items-stretch justify-between   flex-row-reverse py-4  bg-[#080808] border border-[#29292979] rounded-md text-start'
           >
-            <section className='flex-2 flex justify-center'>
-              <Image src={`/${item.name}`} alt={item.name} width={150} height={150} className='w-full object-contain rounded-lg mr-4' />
+            <section className='flex-auto flex justify-center mr-4'>
+              <Image src={`/${item.name}`} alt={item.name} width={400} height={400} className=' object-cover rounded-lg' />
             </section>
-            <section className='flex flex-col rounded-md flex-1 gap-4 pl-4'>
-              <h1 className='text-2xl text-white font-bold'>{item.Title}</h1>
-              <h2 className='text-gray-400 w-10/12 mr-6 text-justify break-all indent-4'>{item.subtitle}</h2>
+            <section className='flex flex-col rounded-md flex-auto gap-4 pl-4  w-3/5 h-2/3'>
+              <h1 className='text-xl text-white font-bold'>{item.Title}</h1>
+              <h2 className='text-gray-400 w-11/12 text-justify break-all indent-0'>{item.subtitle}</h2>
               <article className='gap-5 flex'>
                 <Link href={item.linkGit} target='_blank' className='flex text-xs gap-3 items-center hover:bg-orange-400 hover:p-1 hover:rounded-lg transition-all duration-300 delay-100'>
                   <p><SiCodereview /></p>
