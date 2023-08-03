f9510129153acd1b06fefa99fd182683df65b5d5
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2023-08-03 14:57:03 -0300

    responsive: tela do work tablet

--- components/Work.tsx
+++ components/Work.tsx
@@ -27,30 +27,33 @@ const Work = () => {
     },
   };
   return (
-    <div className='text-white w-full justify-around flex items-center h-full max-ipad:h-full'>
+    <div className='text-white w-full justify-around flex items-center h-full max-ipad:h-full max-tablet:flex-col max-tablet:row'>
       <div className='max-ipad:hidden'>
         <p className='text-[25rem] text-[#3a3a3a79] font-thin'>&#123;</p>
       </div>
+      <h1 className='text-3xl my-3 max-ipad:my-1 max-ipad:text-xl tablet:hidden'>
+          Projects<span className='text-[#21d19f] text-2xl'>.</span>
+        </h1>
       <motion.div
         variants={container}
         initial='hidden'
         animate='visible'
-        className='flex flex-col h-full text-center gap-2 w-full ml-4 max-ipad:ml-0 max-ipad:max-h-full'
+        className='flex flex-col h-full text-center gap-2 w-full ml-4 max-ipad:ml-0 max-ipad:max-h-full max-tablet:flex-row '
       >
-        <h1 className='text-3xl my-3 max-ipad:my-1 max-ipad:text-xl'>
+        <h1 className='text-3xl my-3 max-ipad:my-1 max-ipad:text-xl max-tablet:hidden'>
           Projects<span className='text-[#21d19f] text-2xl'>.</span>
         </h1>
         {work.map((item) => (
           <motion.div
             key={item.id}
             variants={itemVariants}
-            className=' flex w-full flex-row-reverse py-2  bg-[#080808] rounded-md text-start  max-ipad:h-5/6'
+            className=' flex w-full flex-row-reverse py-2  bg-[#080808] rounded-md text-start  max-ipad:h-5/6 max-tablet:flex-col max-tablet:w-1/3 max-tablet:h-full max-tablet:gap-4'
           >
             <motion.section
               variants={itemVariants}
               initial='hidden'
               animate='visible'
-              className='flex-auto flex justify-center mr-4 max-ipad:mr-2'
+              className='flex-auto flex justify-center mr-4 max-ipad:mr-2 max-tablet:mx-1'
             >
               <Image
                 src={`/${item.name}`}
@@ -64,35 +67,36 @@ const Work = () => {
               variants={itemVariants}
               initial='hidden'
               animate='visible'
-              className='flex flex-col rounded-md flex-auto gap-4 max-ipad:gap-3 pl-4 max-ipad:pl-2  w-3/5 h-2/3 max-ipad:w-full max-ipad:h-full'
+              className='flex flex-col rounded-md flex-auto gap-4 max-ipad:gap-3 pl-4 max-ipad:pl-2  w-3/5 h-2/3 max-ipad:w-full max-ipad:h-full max-tablet:pl-1'
             >
               <h1 className='text-xl text-white font-bold max-ipad:text-base'>{item.Title}</h1>
               <h2 className='text-gray-400 w-11/12 max-ipad:text-xs text-justify break-all indent-0'>
                 {item.subtitle}
               </h2>
-              <article className='gap-5 flex'>
+              <article className='gap-5 flex max-tablet:gap-2'>
                 <Link
                   href={item.linkGit}
                   target='_blank'
-                  className='flex text-xs gap-3 items-center  max-ipad:bg-orange-400 max-ipad:text-black max-ipad:rounded-lg max-ipad:px-1 hover:bg-orange-400 hover:p-1 hover:rounded-lg transition-all duration-300 delay-100'
+                  className='flex text-xs gap-3 items-center  max-ipad:bg-orange-400 max-ipad:text-black max-ipad:rounded-lg max-ipad:px-1 hover:bg-orange-400 hover:p-1 hover:rounded-lg transition-all duration-300 delay-100 max-tablet:gap-1 '
                 >
-                  <p>
+                  <p className='max-tablet:text-xs'>
                     <SiCodereview />
                   </p>
-                  {item.github}
+                  <p className='max-tablet:text-xs  max-tablet:text-center'>{item.github}</p>
+                  
                 </Link>
                 <Link
                   href={item.linkDeploy}
                   target='_blank'
-                  className=' flex text-xs gap-2 items-center hover:text-black hover:bg-green-400 max-ipad:bg-green-400 max-ipad:text-black max-ipad:rounded-lg max-ipad:px-1  hover:p-1 hover:rounded-lg transition-all duration-300 delay-100 '
+                  className=' flex text-xs gap-2 items-center hover:text-black hover:bg-green-400 max-ipad:bg-green-400 max-ipad:text-black max-ipad:rounded-lg max-ipad:px-1  hover:p-1 hover:rounded-lg transition-all duration-300 delay-100 max-tablet:mx-1'
                 >
                   <p className='text-lg'>
                     <MdPreview />
                   </p>
-                  <p>{item.deploy}</p>
+                  <p className='max-tablet:text-xs max-tablet:text-center'>{item.deploy}</p>
                 </Link>
               </article>
-              <article className=' flex items-center gap-3'>
+              <article className=' flex items-center gap-3 max-tablet:flex-wrap max-tablet:gap-1'>
                 {item.hastags.map((tag, index) => (
                   <p key={index} className='text-[10px] max-ipad:text-[6px] folder p-1 px-2 rounded-2xl'>
                     {tag}
