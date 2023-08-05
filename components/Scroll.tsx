f56d54c4e79d5c7e748cb17e5fbe6f83efd71d54
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2023-08-04 21:58:03 -0300

    finish

--- components/Scroll.tsx
+++ components/Scroll.tsx
@@ -41,16 +41,14 @@ const Scroll: FC<IMain> = ({ Hello, Services, Work, Skills, Certificates }) => {
     setWork(false);
     setSkills(false);
     setCertificate(false);
-
   };
-  
+
   const handleClickServices = () => {
     setHello(false);
     setServices(true);
     setWork(false);
     setSkills(false);
     setCertificate(false);
-
   };
 
   const handleClickWork = () => {
@@ -59,7 +57,6 @@ const Scroll: FC<IMain> = ({ Hello, Services, Work, Skills, Certificates }) => {
     setWork(true);
     setSkills(false);
     setCertificate(false);
-
   };
 
   const handleClickSkills = () => {
@@ -68,7 +65,6 @@ const Scroll: FC<IMain> = ({ Hello, Services, Work, Skills, Certificates }) => {
     setWork(false);
     setSkills(true);
     setCertificate(false);
-
   };
 
   const handleClickCertificates = () => {
@@ -77,7 +73,7 @@ const Scroll: FC<IMain> = ({ Hello, Services, Work, Skills, Certificates }) => {
     setWork(false);
     setSkills(false);
     setCertificate(true);
-  }
+  };
 
   useEffect(() => {
     setVisibleItems((prevVisibleItems) => ({ ...prevVisibleItems, hello: true }));
@@ -86,7 +82,10 @@ const Scroll: FC<IMain> = ({ Hello, Services, Work, Skills, Certificates }) => {
     const timeoutIds = [
       setTimeout(
         () =>
-          setVisibleItems((prevVisibleItems) => ({...prevVisibleItems, services: true })),
+          setVisibleItems((prevVisibleItems) => ({
+            ...prevVisibleItems,
+            services: true,
+          })),
         delay * 1
       ),
       setTimeout(
@@ -101,7 +100,10 @@ const Scroll: FC<IMain> = ({ Hello, Services, Work, Skills, Certificates }) => {
       ),
       setTimeout(
         () =>
-          setVisibleItems((prevVisibleItems) => ({ ...prevVisibleItems, certificates: true })),
+          setVisibleItems((prevVisibleItems) => ({
+            ...prevVisibleItems,
+            certificates: true,
+          })),
         delay * 4
       ),
     ];
@@ -113,112 +115,115 @@ const Scroll: FC<IMain> = ({ Hello, Services, Work, Skills, Certificates }) => {
     useSpring({
       opacity: visible ? 1 : 0,
       to: { opacity: visible ? 1 : 0 },
-      config: { duration: 500 },    
+      config: { duration: 500 },
     });
 
   return (
     <div className=' flex p-4 justify-between items-center z-0 h-full w-4/5  flex-auto max-iphone:flex-col max-tab max-iphone:h-full  max-ipad:ml-2 max-ipad:w-4/5 max-tablet:w-full max-tablet:ml-0  max-iphone:mt-2 max-tablet:flex-col'>
       <ul className='text-2xl gap-10 max-tablet:gap-2 transform flex flex-col text-white text-center p-4 max-iphone:w-full max-iphone:flex-row max-iphone:gap-2 max-iphone:mt-6 max-smiphone:gap-2 max-smiphone:justify-center max-iphone:p-2 max-iphone:text-lg max-smiphone:mt-2  max-tablet:w-full max-tablet:flex-row max-tablet:mt-2 max-tablet:flex max-tablet:justify-center max-tablet:items-baseline max-tablet:p-0'>
         {hello ? (
-          <Link
-            href='#hello'
-            onClick={handleClickHello}
-          >
-            <animated.li style={IconAnimation(visibleItems.hello)}  className='bg-[#21D19F] transition-all flex
-        justify-center rotate-[-15deg]  max-iphone:rotate-0 max-tablet:rotate-0 rounded-sm text-lg py-1 max-iphone:px-2 max-iphone:border-r-[#21d19f] max-iphone:border-r-[1px] tablet:border-r-[#21d19f] max-tablet:border-r-[1px] max-iphone:text-base max-tablet:justify-center max-smiphone:text-xs max-iphone:pr-2  max-tablet:px-4'>{Hello}</animated.li>
+          <Link href='#hello' onClick={handleClickHello}>
+            <animated.li
+              style={IconAnimation(visibleItems.hello)}
+              className='bg-[#21D19F] transition-all flex
+        justify-center rotate-[-15deg]  max-iphone:rotate-0 max-tablet:rotate-0 rounded-sm text-lg py-1 max-iphone:px-2 max-iphone:border-r-[#21d19f] max-iphone:border-r-[1px] tablet:border-r-[#21d19f] max-tablet:border-r-[1px] max-iphone:text-base max-tablet:justify-center max-smiphone:text-xs max-iphone:pr-2  max-tablet:px-4'
+            >
+              {Hello}
+            </animated.li>
           </Link>
         ) : (
-          <Link
-            href='#hello'
-            onClick={handleClickHello}
-            className=' transition-all flex
+          <Link href='#hello' onClick={handleClickHello}>
+            <animated.li
+              style={IconAnimation(visibleItems.hello)}
+              className=' transition-all flex
         justify-start  pl-6 max-iphone:pl-2 rotate-[-15deg]  max-iphone:border-r-[#21d19f] max-iphone:border-r-[1px] max-tablet:border-[#21d19f] max-tablet:border-[1px]   max-iphone:text-base max-smiphone:text-xs max-iphone:pr-2  max-iphone:rotate-0 max-tablet:rotate-0 max-tablet:flex max-tablet:px-4 max-tablet:justify-center max-tablet:items-center max-tablet:text-center  rounded-sm text-lg'
-          >
-            <animated.li style={IconAnimation(visibleItems.hello)}>{Hello}</animated.li>
+            >
+              {Hello}
+            </animated.li>
           </Link>
         )}
         {services ? (
-          <Link
-            href='#services'
-            onClick={handleClickServices}
-            className='bg-[#21D19F] py-1 flex transition-all rounded-sm rotate-[-10deg]  max-iphone:border-r-[#21d19f] max-iphone:border-r-[1px] max-tablet:border-[#21d19f] max-tablet:border-[1px]   max-iphone:text-base max-smiphone:text-xs max-iphone:pr-2 max-smiphone:px-2  max-iphone:rotate-0 max-tablet:rotate-0 rounded-sm  px-8 max-iphone:px-0 ml-2 max-tablet:ml-0 max-iphone:-ml-0 items-center justify-center text-lg '
-          >
-            <animated.li style={IconAnimation(visibleItems.services)}>
+          <Link href='#services' onClick={handleClickServices}>
+            <animated.li
+              style={IconAnimation(visibleItems.services)}
+              className='bg-[#21D19F] py-1 flex transition-all rounded-sm rotate-[-10deg]  max-iphone:border-r-[#21d19f] max-iphone:border-r-[1px] max-tablet:border-[#21d19f] max-tablet:border-[1px]   max-iphone:text-base max-smiphone:text-xs max-iphone:pr-2 max-smiphone:px-2  max-iphone:rotate-0 max-tablet:rotate-0 px-8 max-iphone:px-0 ml-2 max-tablet:ml-0 max-iphone:-ml-0 items-center justify-center text-lg '
+            >
               {Services}
             </animated.li>
           </Link>
         ) : (
-          <Link
-            href='#services'
-            onClick={handleClickServices}
-            className='hover:bg-[#36363644] transition-all hover:rounded-sm rotate-[-10deg]  max-iphone:border-r-[#21d19f] max-iphone:border-r-[1px] max-tablet:border-[#21d19f] max-tablet:border-[1px]   max-iphone:text-base max-smiphone:text-xs max-iphone:px-2  max-iphone:rotate-0 max-tablet:rotate-0 rounded-sm  ml-4 max-iphone:ml-0 max-tablet:ml-0 px-6  items-center justify-center text-lg hover:justify-center hover:items-center hover:text-center hover:flex'
-          >
-            <animated.li style={IconAnimation(visibleItems.services)}>
+          <Link href='#services' onClick={handleClickServices}>
+            <animated.li
+              style={IconAnimation(visibleItems.services)}
+              className='hover:bg-[#36363644] transition-all hover:rounded-sm rotate-[-10deg]  max-iphone:border-r-[#21d19f] max-iphone:border-r-[1px] max-tablet:border-[#21d19f] max-tablet:border-[1px]   max-iphone:text-base max-smiphone:text-xs max-iphone:px-2  max-iphone:rotate-0 max-tablet:rotate-0 rounded-sm  ml-4 max-iphone:ml-0 max-tablet:ml-0 px-6  items-center justify-center text-lg hover:justify-center hover:items-center hover:text-center hover:flex'
+            >
               {Services}
             </animated.li>
           </Link>
         )}
         {work ? (
-          <Link
-            href='#Work'
-            onClick={handleClickWork}
-            className='bg-[#21D19F] py-1 transition-all rounded-sm px-6 max-iphone:px-0 max-iphone:border-r-[#21d19f] max-iphone:border-r-[1px] tablet:border-r-[#21d19f] max-tablet:border-r-[1px]   max-iphone:text-base max-smiphone:text-xs max-iphone:pr-2 items-center justify-center text-lg max-smiphone:px-2'
-          >
-            <animated.li style={IconAnimation(visibleItems.work)}>{Work}</animated.li>
+          <Link href='#Work' onClick={handleClickWork}>
+            <animated.li
+              style={IconAnimation(visibleItems.work)}
+              className='bg-[#21D19F] py-1 transition-all rounded-sm px-6 max-iphone:px-0 max-iphone:border-r-[#21d19f] max-iphone:border-r-[1px] tablet:border-r-[#21d19f] max-tablet:border-r-[1px]   max-iphone:text-base max-smiphone:text-xs max-iphone:pr-2 items-center justify-center text-lg max-smiphone:px-2'
+            >
+              {Work}
+            </animated.li>
           </Link>
         ) : (
-          <Link
-            href='#Work'
-            onClick={handleClickWork}
-            className='hover:bg-[#36363644] transition-all hover:rounded-sm flex pl-2 justify-center text-lg max-iphone:border-r-[#21d19f] max-iphone:border-r-[1px] tablet:border-r-[#21d19f] max-tablet:border-r-[1px]  max-iphone:text-base max-smiphone:text-xs max-iphone:flex max-iphone:justify-center max-iphone:pl-2 max-smiphone:pr-2 max-tablet:border-[#21d19f] max-tablet:border-[1px] max-tablet:px-2 rounded-sm'
-          >
-            <animated.li style={IconAnimation(visibleItems.work)}>{Work}</animated.li>
+          <Link href='#Work' onClick={handleClickWork}>
+            <animated.li
+              style={IconAnimation(visibleItems.work)}
+              className='hover:bg-[#36363644] transition-all hover:rounded-sm flex pl-2 justify-center text-lg max-iphone:border-r-[#21d19f] max-iphone:border-r-[1px] tablet:border-r-[#21d19f] max-tablet:border-r-[1px]  max-iphone:text-base max-smiphone:text-xs max-iphone:flex max-iphone:justify-center max-iphone:pl-2 max-smiphone:pr-2 max-tablet:border-[#21d19f] max-tablet:border-[1px] max-tablet:px-2 rounded-sm'
+            >
+              {Work}
+            </animated.li>
           </Link>
         )}
         {skills ? (
-          <Link
-            href='#skills'
-            onClick={handleClickSkills}
-            className='rotate-[10deg]   max-iphone:border-r-[#21d19f] max-iphone:border-r-[1px] max-tablet:border-[#21d19f] max-tablet:border-[1px]   max-iphone:text-base max-smiphone:text-xs max-iphone:pr-2 max-iphone:rotate-0 max-tablet:rotate-0 rounded-sm py-1 bg-[#21D19F] transition-all  text-lg max-smiphone:px-2 max-tablet:px-4'
-          >
-            <animated.li style={IconAnimation(visibleItems.skills)}>{Skills}</animated.li>
+          <Link href='#skills' onClick={handleClickSkills}>
+            <animated.li
+              style={IconAnimation(visibleItems.skills)}
+              className='rotate-[10deg]   max-iphone:border-r-[#21d19f] max-iphone:border-r-[1px] max-tablet:border-[#21d19f] max-tablet:border-[1px]   max-iphone:text-base max-smiphone:text-xs max-iphone:pr-2 max-iphone:rotate-0 max-tablet:rotate-0 rounded-sm py-1 bg-[#21D19F] transition-all  text-lg max-smiphone:px-2 max-tablet:px-4'
+            >
+              {Skills}
+            </animated.li>
           </Link>
         ) : (
-          <Link
-            href='#skills'
-            onClick={handleClickSkills}
-            className='rotate-[10deg]   max-iphone:border-r-[#21d19f] max-iphone:border-r-[1px] max-tablet:border-[#21d19f] max-tablet:border-[1px]   max-iphone:text-base max-smiphone:text-xs max-iphone:pr-2 max-iphone:rotate-0 max-tablet:rotate-0  hover:bg-[#36363644] transition-all hover:rounded-sm text-lg max-tablet:px-2 items-center rounded-sm'
-          >
-            <animated.li style={IconAnimation(visibleItems.skills)}>{Skills}</animated.li>
+          <Link href='#skills' onClick={handleClickSkills}>
+            <animated.li
+              style={IconAnimation(visibleItems.skills)}
+              className='rotate-[10deg]   max-iphone:border-r-[#21d19f] max-iphone:border-r-[1px] max-tablet:border-[#21d19f] max-tablet:border-[1px]   max-iphone:text-base max-smiphone:text-xs max-iphone:pr-2 max-iphone:rotate-0 max-tablet:rotate-0  hover:bg-[#36363644] transition-all hover:rounded-sm text-lg max-tablet:px-2 items-center rounded-sm'
+            >
+              {Skills}
+            </animated.li>
           </Link>
         )}
         {certificate ? (
-            <Link
-              href='#certificates'
-              onClick={handleClickCertificates}
+          <Link href='#certificates' onClick={handleClickCertificates}>
+            <animated.li
+              style={IconAnimation(visibleItems.certificates)}
               className='bg-[#21D19F] transition-all py-1 rotate-[15deg]  max-iphone:text-base max-smiphone:text-xs max-iphone:pr-2  max-iphone:rotate-0 max-tablet:rotate-0 rounded-sm  px-8 max-smiphone:px-2  items-center justify-center text-lg max-smiphone:flex-wrap'
             >
-              <animated.li style={IconAnimation(visibleItems.certificates)}>
-                {Certificates}
-              </animated.li>
-            </Link>
+              {Certificates}
+            </animated.li>
+          </Link>
         ) : (
-            <Link
-              href='#certificates'
-              onClick={handleClickCertificates}
+          <Link href='#certificates' onClick={handleClickCertificates}>
+            <animated.li
+              style={IconAnimation(visibleItems.certificates)}
               className='text-lg hover:bg-[#36363644] transition-all hover:rounded-sm rotate-[15deg]    max-iphone:text-base max-smiphone:text-xs max-iphone:pr-2  max-iphone:rotate-0 max-tablet:rotate-0 rounded-sm flex justify-center pr-3 max-tablet:border-[#21d19f] max-tablet:border-[1px] max-tablet:px-2 items-center max-smiphone:flex-wrap'
             >
-              <animated.li style={IconAnimation(visibleItems.certificates)}>
-                {Certificates}
-              </animated.li>
-            </Link>
+              {Certificates}
+            </animated.li>
+          </Link>
         )}
       </ul>
       {hello && <HelloComponent />}
       {services && <ServicesComponent />}
       {work && <WorkComponent />}
       {skills && <SkillsComponent />}
-      {certificate && <CertificateComponent  />}
+      {certificate && <CertificateComponent />}
     </div>
   );
 };
